9e8a1d459d9224fd8e2a9b2c53b0a04ef059c3a6
mdshoaib77 <email4966@gmail.com>
2026-04-03 21:15:54 +0600

    ALhamdulliah

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -33,7 +33,7 @@ const Testimonials = () => {
     >
       <h3 className="bold-32 text-center">Testimonial</h3>
       <p className="text-center max-w-lg m-auto text-gray-30 py-6">
-       Best car ever i received in my life
+       Best car ever i received in my life form this website 
       </p>
       {/* slider */}
       <Slider {...settings}>
