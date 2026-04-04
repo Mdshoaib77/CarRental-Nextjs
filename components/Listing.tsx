dd887f8887d22e39ddec101063c2f3bdbb54aeee
mdshoaib77 <email4966@gmail.com>
2026-04-04 19:54:19 +0600

    Update car catalog text

--- components/Listing.tsx
+++ components/Listing.tsx
@@ -9,10 +9,11 @@ const Listing = () => {
       <div className="max-container padding-container !max-w-[1300px]">
         <div className="flex flex-col items-start justify-start gap-y-2.5 text-black">
           <h3 className="h3 mt-8">Car Catalogue</h3>
-          <p className="max-w-[33rem]">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis
-            molestias rerum voluptatem!
-          </p>
+       <p className="max-w-[33rem]">
+  Explore our carefully curated collection of vehicles, each fully inspected 
+  for comfort, safety, and performance. Book your ideal car and hit the road 
+  with confidence.
+</p>
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4  w-full gap-8 pt-14 gap-y-16">
             {CARS.map((car) => (
               <CarItem
