2a8b773a51ec5e4c16dda6ff530ccbee24686b38
Md Shoaib <email4966@gmail.com>
2026-06-06 14:19:41 +0600

    Fix typo in comment in Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -42,7 +42,7 @@ const Header = () => {
             className="h-auto w-28"
           />
         </Link>
-        {/* nav linksj h*/}
+        {/* nav linksj nh*/}
         <Nav
           containerStyles={`${
             menuOpened
