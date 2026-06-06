aa49e2600c695b659009471d7bc659b3931c998d
Md Shoaib <email4966@gmail.com>
2026-06-06 14:22:15 +0600

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -42,7 +42,7 @@ const Header = () => {
             className="h-auto w-28"
           />
         </Link>
-        {/*  nav jlinksj nh*/}
+        {/*  hnav jlinksj nh*/}
         <Nav
           containerStyles={`${
             menuOpened
