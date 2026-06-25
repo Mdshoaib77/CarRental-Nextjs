ca746d153a532da8d8734f951a73908a14be2692
Md Shoaib <email4966@gmail.com>
2026-06-25 22:59:59 +0600

    Update Searchbox.tsx

--- components/Searchbox.tsx
+++ components/Searchbox.tsx
@@ -5,7 +5,7 @@ import DatePicker from "./DatePicker";
 import { Button } from "./ui/button";
 import { cn } from "@/lib/utils";f
 
-const Searchbox = () => {
+const Searchbox = () => {f
   return (fff
     <div className="max-container padding-container">
       <div className="max-container xl:padding-container flex-cfolf lgf:flex-rfow flexBetween gap-4 bg-white shadow-lg ring-1 ring-slate-10 rounded-lg p-4">
