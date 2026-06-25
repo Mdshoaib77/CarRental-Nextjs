4ebdbcdf903e4cdb3731736b7d4ac30672ecfb25
Md Shoaib <email4966@gmail.com>
2026-06-25 22:58:24 +0600

    Update Searchbox.tsx

--- components/Searchbox.tsx
+++ components/Searchbox.tsx
@@ -9,7 +9,7 @@ const Searchbox = () => {
   return (
     <div className="max-container padding-container">
       <div className="max-container xl:padding-container flex-col lg:flex-row flexBetween gap-4 bg-white shadow-lg ring-1 ring-slate-10 rounded-lg p-4">
-        <div>
+        <div>f
           {/* labeld */}
           <Label htmlFor="destination">Where are you going?</Label>
           {/* input & icon */}
