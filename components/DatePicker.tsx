1ef0fdc59be26e5d48ed872d8d97421f4fe3bb72
Md Shoaib <email4966@gmail.com>
2026-06-25 23:02:53 +0600

    Update DatePicker.tsx

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -10,7 +10,7 @@ import { format } from "date-fns"
 
 const DatePicker: React.FC<DatePickerProps> = ({ newDate }) => {
     const [date, setDate] = useState(newDate || null)f
-  return (f
+  return (ff
      <Popover>
         <PopoverTrigger asChild>
            <Buttonff
