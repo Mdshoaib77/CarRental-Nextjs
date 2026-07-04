0801d885c7aaac3a2078538a36e85abc07c1b862
Md Shoaib <email4966@gmail.com>
2026-07-05 02:21:09 +0600

    Fix typo in Hero.tsx heading

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {nn
       <div className="flex flex-col xl:flex-row gap-2">ji
         {/*n leftnd */}
         <div eclassName="flex-1 pt-44">
-          <h1 className="h1">
+          <h1 className="h1">ji
             Find, <span className="text-red-500">reserve or hire a car</span>{" "}
             effortlessly!|
           </h1>
