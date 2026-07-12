a1b816fbcd45f053ac03eb685f3fdd8448719037
Md Shoaib <email4966@gmail.com>
2026-07-12 23:45:01 +0600

    Update Testimonials.tsx

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -3,7 +3,7 @@ import { TESTIMONIALS } from "@/constants";
 import Image from "next/image";
 import { RiDoubleQuotesR } from "react-icons/ri";
 import Slider from "react-slick";
-jjhgdfgfds
+jjhgdfgfdsgds
 const Testimonials = () => {hdfggd
   const settings = {hgdsbgdshfgdgrds
     autoplay: true,bgdsgfds
