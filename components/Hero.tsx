284212f6b559fb31f8bb81a74b02d5be02191f63
Md Shoaib <email4966@gmail.com>
2026-07-12 22:30:37 +0600

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Searchbox from "./Searchbox";
 
 const Hero = () => {nn
-  return (ghfd
+  return (ghfdhgfd
     <section className="max-container padding-container z-0 relative xl:h-[95vh]" id="home">
       <div className="flex flex-col xl:flex-row gap-2">jihgfd
         {/*n leftnd */}hfd
