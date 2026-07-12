7665896460ead91e17ecb2d40642571a691be4f5
Md Shoaib <email4966@gmail.com>
2026-07-12 23:45:30 +0600

    Update Testimonials.tsx

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -9,7 +9,7 @@ const Testimonials = () => {hdfggd
     autoplay: true,bgdsgfdsgdfs
     infinite: true,hgdfgfdgs
     slidesToShow: 3,fdsgter
-    slidesToScroll: 1,gdsbvcxgdfs
+    slidesToScroll: 1,gdsbvcxgdfsgdf
     responsive: [hgfgdxmhgfdhgfd
    hfd   {gfdsgfdbkugdshgfdgds
         breakpoint: 1280,gfghkunchvgfdhdfg
