d2515d4bc0e4148eb756c67b61ad863ee3291cf1
Md Shoaib <email4966@gmail.com>
2026-07-12 22:31:45 +0600

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {nn
          <p className="mb-4">ghfd
   Enjoy hassle-free car rentals with flexible options, competitive pricing, hgfdhgfdighfd
   and a seamless booking experience tailored for your convenience.fd
-</p>
+</p>hgfd
           <div className="flex gap-2">gfhfgd
             <Link href={"/"}>
               <Imageg
