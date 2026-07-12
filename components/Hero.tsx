672c64e4728febe39f816ead965f67940248fc08
Md Shoaib <email4966@gmail.com>
2026-07-12 22:31:17 +0600

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,8 +8,7 @@ const Hero = () => {nn
       <div className="flex flex-col xl:flex-row gap-2">jihgfd
         {/*n leftnd */}hfdhgdf
         <div eclassName="flex-1 pt-44">hdfgjfg
-          <h1 className="h1">jihgfdhdfgjhfghfhdf
-            Find, <span className="text-red-500">reserve or hire a car</span>{" ghf}ghdfhgdhgfghfdhgdf
+          <h1 className="h1">jihgfdhdfgjhfghfhdfhgfd/span>{" ghf}ghdfhgdhgfghfdhgdf
             effortlessly!|hdfhgfdhgdfhgdfhgfdhgf
          <p className="mb-4">ghfd
   Enjoy hassle-free car rentals with flexible options, competitive pricing, hgfdhgfdighfd
