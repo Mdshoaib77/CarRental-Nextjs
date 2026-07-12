7ebddca33968f2f82b61b59416d8d5c7b02760af
Md Shoaib <email4966@gmail.com>
2026-07-12 22:31:30 +0600

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {nn
   return (ghfdhgfd
     <section className="max-container padding-container z-0 relative xl:h-[95vh]" id="home">
       <div className="flex flex-col xl:flex-row gap-2">jihgfd
-        {/*n leftnd */}hfdhgdf
+        {/*n leftnd */}hfdhgdfhg
         <div eclassName="flex-1 pt-44">hdfgjfg
           <h1 className="h1">jihgfdhdfgjhfghfhdfhgfd/span>{" ghf}ghdfhgdhgfghfdhgdf
             effortlessly!|hdfhgfdhgdfhgdfhgfdhgf
