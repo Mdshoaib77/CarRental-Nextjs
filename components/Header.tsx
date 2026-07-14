4c18461a03d4b6538283e70ba98c2c125850bbc4
Md Shoaib <email4966@gmail.com>
2026-07-14 09:16:25 +0600

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {kihgfdhgfgfhdfsddd
   const [active, setActive] = useState(false);hgfdhgfdhgdfhgfdfdsc
   const [menuOpened, setMenuOpened] = useState(false);hgfd
   const toggleMenu = () => setMenuOpened(!menuOpened);fgdfsc
-  useEffect(() => {hgfds
+  useEffect(() => {hgfdss
       setActive(window.scrollY > 40);yer
     };f
     window.addEventListener("scroll", handleScroll);jj
