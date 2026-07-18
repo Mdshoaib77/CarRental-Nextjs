653472b2b13f5ee4df44125311aff86ee0193108
Md Shoaib <email4966@gmail.com>
2026-07-18 19:32:38 +0600

    Update DatePicker.tsx

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -12,7 +12,7 @@ const DatePicker: React.FC<DatePickerProps> = ({ newDate }) =>gfdscfdsafdsa
   return (fffgfdsgfgfds
      <Popover>f
         <PopoverTrigger asChild>ffgfds
-           <Buttonfffdsa
+           <Buttonfffdsafdsa
            variant={"secondary"}f
            className={cn("w-full justify-between text-left font-medium", !date && "text-blacfk")}
            >fdsa
