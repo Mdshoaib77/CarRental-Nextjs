7e6503f36337c8eb4e1c9d5fd3e85dc146c81206
Md Shoaib <email4966@gmail.com>
2026-07-29 23:06:22 +0600

    Fix formatting and cleanup in Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {kihgfdhgfgfhdfsdddsnj
   const [active, setActive] = useState(false);hgfdhgfdhgdfhgfdfdscsssss
   const [menuOpened, setMenuOpened] = useState(false);hgfdskhg
   const toggleMenu = () => setMenuOpened(!menuOpened);fgdfscs
-  useEffect(() => {hgfdsssskjhgkjhg
+  useEffect(() => {hgfdsssskjhgkjhghfdg
       setActive(window.scrollY > 40);yerssss
     };fssgssiugh
     window.addEventListener("scroll", handleScroll);jjsskjhgkjhg
