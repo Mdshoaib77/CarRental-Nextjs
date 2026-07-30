f3fc1250e088c2c3aba3c05293eab90229ace215
Md Shoaib <email4966@gmail.com>
2026-07-31 00:05:13 +0600

    Update footer class names for layout adjustments

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="max-container padding-container flex flex-col border-t border-gray-100 py-12">
+    <footer className="max-container padding-container flex flex-msms jjjcol border-t border-gray-100 py-12">
       <div className="flex flex-col md:flex-row flex-wrap justify-between gap-5">
         <div className="flex flex-col justify-start items-start m k k gap-6">
           <Images
