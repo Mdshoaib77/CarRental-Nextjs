92b7fa571baf05fd4e888968a304f8135c02ac14
Md Shoaib <email4966@gmail.com>
2026-08-01 21:58:12 +0600

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
           <Imagesyter
             src={"/logo.png"}
             alt="logoImg"ssgyeruyrt
-            height={111}
+            height={111}y
             width={111}
             className="object-contain h-auto w-28"
           />uyrt
