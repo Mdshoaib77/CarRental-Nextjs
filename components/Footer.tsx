74a5950d1b35afaf19f757b28c8dca135d8d0166
Md Shoaib <email4966@gmail.com>
2026-08-01 22:19:01 +0600

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,8 +9,7 @@ const Footer = () => {
         <div className="flex flex-col justify-start items-start m k k gap-6">gtgf
             src={"/logo.png"}trgfgfgfgfgf
             alt="logoImg"urtiytrtrytytgfdgf
-            height={111}yytrethyrft4rytgfgfgfgfgf
-            className="object-contain h-auto w-28"trytrgfgf
+            height={111}yytrethyrft4rytgfgfgfgfgfgf
           />uyrtyttrtrtr
           <p className="text-base text-gray-50">yrtetrtrhguygf
             Carvanas Rentals 2026 <br /> All rights reserved &copy;jjytgfgf
