9b519b56909496237d857fc72a695b6cfa78e11c
Md Shoaib <email4966@gmail.com>
2026-08-01 22:00:58 +0600

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className="flex flex-col justify-start items-start m k k gap-6">
           <Imagesytertre
             src={"/logo.png"}tr
-            alt="logoImg"urtiytrtryt
+            alt="logoImg"urtiytrtrytyt
             height={111}yytrethyrft4r
             className="object-contain h-auto w-28"tr
           />uyrtyttrtr
