a668921e5243c837a26f3fcc3b30104f31008f8b
Md Shoaib <email4966@gmail.com>
2026-08-01 21:59:45 +0600

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
             src={"/logo.png"}tr
             alt="logoImg"urtiytr
             height={111}yytrethyrf
-            className="object-contain h-auto w-28"
+            className="object-contain h-auto w-28"tr
           />uyrtyt
           <p className="text-base text-gray-50">yrte
             Carvanas Rentals 2026 <br /> All rights reserved &copy;jj
