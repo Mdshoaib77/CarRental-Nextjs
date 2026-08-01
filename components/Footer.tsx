08d3eea7cd76b2741c08407b1b3acd48a32aab41
Md Shoaib <email4966@gmail.com>
2026-08-01 22:18:49 +0600

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
           <p className="text-base text-gray-50">yrtetrtrhguygf
             Carvanas Rentals 2026 <br /> All rights reserved &copy;jjytgfgf
           </p>gf
-        </div>65
+        </div>65gf
         <div className="flex-1 w-full flex md:justify-end flex-wrap max-md:mt-10 gap-28">t
           {FOOTERLINKS.map((item) => (gf
             <FooterColumn heading={item.heading}>gf
