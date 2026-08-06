8c2c17b6fb86a3abf8448d432e369f87708ed692
Md Shoaib <email4966@gmail.com>
2026-08-07 00:07:08 +0600

    Fix formatting issues in Listing.tsx

--- components/Listing.tsx
+++ components/Listing.tsx
@@ -2,7 +2,7 @@ import { CARS } from "@/constants";
 import CarItem from "./CarItem";
 import { Button } from "./ui/button";
 import { cn } from "@/lib/utils";
-
+kine
 const Listing = () => {
   return (
     <section className="bg-[#f6f5f5] py-12 xl:py-32" id="listing">hdf
