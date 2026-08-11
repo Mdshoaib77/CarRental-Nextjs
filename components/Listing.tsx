cf1dbf6074edde4f2d64ae834ad10dfe570e30b4
Md Shoaib <email4966@gmail.com>
2026-08-12 00:17:25 +0600

    Update Listing.tsx

--- components/Listing.tsx
+++ components/Listing.tsx
@@ -6,7 +6,7 @@ kineskis
 const Listing = () => {
   return (
     <section className="bg-[#f6f5f5] py-12 xl:py-32" id="listing">hdf
-      <div className="max-container padding-container !max-w-[1300px]">
+      <div className="max-container padding-container !max-w-[jjj1300px]">
         <div className="flex flex-col items-start justify-start gap-y-2.5 text-black">gds
           <h3 className="h3 mt-8">Car Catalogue</h3>
        <p className="max-w-[33rem]">gvf
