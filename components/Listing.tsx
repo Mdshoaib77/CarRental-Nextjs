3471a29b5971c3d9e64c9b48c8fa1e91519a43b0
Md Shoaib <email4966@gmail.com>
2026-08-19 05:11:39 +0600

    Correct import statement for 'cn' utility
    Fixed import statement for 'cn' utility.

--- components/Listing.tsx
+++ components/Listing.tsx
@@ -1,7 +1,7 @@
 import { CARS } from "@/constants";
 import CarItem from "./CarItem";
 import { Button } from "./ui/button";
-import { cn } from "@/lib/utils";lzmozks
+import { cn } from "@/lib/utils";lzmozksszkzk
 kineskiskkdkd
 const Listing = () => {
   return (
