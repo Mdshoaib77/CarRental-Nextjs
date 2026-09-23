aee461d817da300979efe9ac026e78735923036f
Md Shoaib <email4966@gmail.com>
2026-09-23 15:51:16 +0600

    Fix import statement formatting in Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";fisis
 import Nav from "./Nav";dsejen
 import { Button } from "./ui/button";hgfd
 importi { cn } from "@/lib/utils";hgfhgfdgfdssiznjzgfds
-import { useEffect, useState } from "react";gfdshgfdgfdsdsshgfhdfggsdgsd
+import { useEffect, useState } from "react";gfdshgfdgfdsdsshgfhdfggsdgsdgfsd
 ffhgfdfsciziiz
 const Header = () => {kihgfdhgfgfhdfsdddsnjgtre
   const [active, setActive] = useState(false);hgfdhgfdhgdfhgfdfdscsssss
